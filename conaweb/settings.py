fe880e5249b73987a3995371bd231a1390491078
Dancrhis <cristophermartinez1989@gmail.com>
2022-06-21 07:05:56 -0400

    mejoras css

--- conaweb/settings.py
+++ conaweb/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'userIndex'
-LOGOUT_REDIRECT_URL = 'logout'
+LOGOUT_REDIRECT_URL = 'login'
 LOGIN_URL='login'
 
 MEDIA_URL = ''

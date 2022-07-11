17ea07b24489578dbc533a6c32f12bb473ddfa34
Dancrhis <cristophermartinez1989@gmail.com>
2022-07-10 22:32:18 -0400

    almost finishing

--- conaweb/settings.py
+++ conaweb/settings.py
@@ -131,6 +131,14 @@ USE_TZ = False
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
+
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER='cristophermartinez1989@gmail.com'
+EMAIL_HOST_PASSWORD="wmbzagmoqejwphfy"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- conaweb/urls.py
+++ conaweb/urls.py
@@ -13,16 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from cgitb import reset
+from re import template
 from django.contrib import admin
 from django.urls import path, include
 from conabom import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("conabom.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="registration/password_reset_form.html"), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"), name="password_reset_done"),
+    path('reset/<uid64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_done.html"),name='password_reset_complete'),
+    
     
     
 

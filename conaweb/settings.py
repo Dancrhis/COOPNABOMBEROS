8890b22dcc3f18ece5b5f2168e469c1b2b8e20da
Dancrhis <cristophermartinez1989@gmail.com>
2022-07-18 22:24:14 -0400

    Update settings.py

--- conaweb/settings.py
+++ conaweb/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import sys
+import dj_database_url
+from django.core.management.utils import get_random_secret_key
 
 
 from django.urls import reverse_lazy
@@ -24,13 +26,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$wl44v2skgyz$&hev=a5i(5k)=d02m2v@zt&)_(-t_h$+q#^29'
-
+#SECRET_KEY = 'django-insecure-$wl44v2skgyz$&hev=a5i(5k)=d02m2v@zt&)_(-t_h$+q#^29'
+#[label django_app/settings.py]
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+#[label django_app/settings.py]
+DEBUG = os.getenv("DEBUG", "False") == "True"
+#[label django_app/settings.py]
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
-ALLOWED_HOSTS = ['arcane-bayou-67214.herokuapp.com','127.0.0.1','https://www.mlcalc.com/', 'clownfish-app-jmsml.ondigitalocean.app','134.122.52.247']
-
+#ALLOWED_HOSTS = ['arcane-bayou-67214.herokuapp.com','127.0.0.1','https://www.mlcalc.com/', 'clownfish-app-jmsml.ondigitalocean.app','134.122.52.247']
+#[label django_app/settings.py]
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 #archivos staticos
 DIR_STATIC=os.path.join(BASE_DIR, "conabom/pages/static/")
 STATICFILES_DIRS=[DIR_STATIC]
@@ -92,20 +100,33 @@ WSGI_APPLICATION = 'conaweb.wsgi.application'
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'coopnabomberos',
-        'USER': 'conabomadmin',
-        'PASSWORD': 'CoopNaBomberos2021',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'coopnabomberos',
+#        'USER': 'conabomadmin',
+#        'PASSWORD': 'CoopNaBomberos2021',
+#        'HOST': 'localhost',
+#        'PORT': '',
+#    }
+#}
 #import dj_database_url
 #db_from_env = dj_database_url.config(conn_max_age=600)
 #DATABASES["default"].update(db_from_env)
-
+#[label django_app/settings.py]
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -141,9 +162,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
-STATIC_URL = '/static/'
 
+STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"

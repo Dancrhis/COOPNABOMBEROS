b18b631bce2f0f397b2edd6199a8889ee6a9e780
Dancrhis <cristophermartinez1989@gmail.com>
2022-07-21 22:12:59 -0400

    Update settings.py

--- conaweb/settings.py
+++ conaweb/settings.py
@@ -163,7 +163,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
